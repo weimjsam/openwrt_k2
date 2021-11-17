93a63dfa6a252a4a6d5cfe4e1a585d6228ef85a1
weimjsam <weimjsam@qq.com>
2021-11-17 15:40:24 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,5 +4,5 @@
  tar -xvf openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar
  cd openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64
  make info
- make image PROFILE=phicomm_psg1218a PACKAGES="uhttpd luci_theme_bootstrap luci-base tc kmod-netem kmod-sched" CONFIG_IPV6=n
+ make image PROFILE=phicomm_psg1218a PACKAGES="libiwinfo-lua luci-base luci-app-firewall luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap wpad-openssl luci-app-ddns -wpad-basic-wolfssl tc kmod-netem kmod-sched" CONFIG_IPV6=n
  
