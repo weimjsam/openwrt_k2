53f35881b060df0c060daa20c0aea16aaf5456d5
weimjsam <weimjsam@qq.com>
2022-03-26 20:11:54 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -8,5 +8,3 @@
  make image PROFILE=d-team_newifi-d2 PACKAGES="vsftpd openssh-sftp-server tc  kmod-ifb kmod-sched kmod-sched-core kmod-netem uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-base luci-app-firewall luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap  luci-i18n-base-zh-cn wpad-openssl luci-app-ddns -wpad-basic-wolfssl -kmod-usb-core -kmod-usb2 -kmod-usb-ohci -kmod-ledtrig-usbdev -kmod-nf-conntrack6 -kmod-nf-ipt6 -libip6tc -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n
 
 # phicomm_psg1218a lenovo_newifi-y1 mt7620 、d-team_newifi-d2 mt7621
-
-
