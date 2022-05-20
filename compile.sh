1a1d6bc453ee86dc96782d16de8caeb061320974
weimjsam <weimjsam@qq.com>
2022-05-20 15:27:04 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -5,6 +5,6 @@
  tar -xvf openwrt-imagebuilder-${version}-ramips-${ramips}.Linux-x86_64.tar
  cd openwrt-imagebuilder-${version}-ramips-${ramips}.Linux-x86_64
  make info
- make image PROFILE=lenovo_newifi-y1 PACKAGES="vsftpd openssh-sftp-server tc  kmod-ifb kmod-sched kmod-sched-core kmod-netem uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-base luci-app-firewall luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap  luci-i18n-base-zh-cn wpad-openssl -luci-app-ddns -ddns-scripts -ddns-scripts_aliyun ddns-scripts_dnspod -wpad-basic-wolfssl -kmod-usb-core -kmod-usb2 -kmod-usb-ohci -kmod-ledtrig-usbdev -kmod-nf-conntrack6 -kmod-nf-ipt6 -libip6tc -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n CONFIG_KERNEL_DEBUG_KERNEL=n CONFIG_KERNEL_ELF_CORE=n
+ make image PROFILE=lenovo_newifi-y1 PACKAGES="vsftpd openssh-sftp-server tc  kmod-ifb kmod-sched kmod-sched-core kmod-netem uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-base luci-app-firewall luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap  luci-i18n-base-zh-cn wpad-openssl -luci-app-ddns -ddns-scripts -ddns-scripts_aliyun ddns-scripts_dnspod -wpad-basic-wolfssl -kmod-usb-core -kmod-usb2 -kmod-usb-ohci -kmod-ledtrig-usbdev -kmod-nf-conntrack6 -kmod-nf-ipt6 -libip6tc -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n CONFIG_STRIP_KERNEL_EXPORTS=y CONFIG_USE_MKLIBS=y
 
 # phicomm_psg1218a lenovo_newifi-y1 mt7620 、d-team_newifi-d2 mt7621
