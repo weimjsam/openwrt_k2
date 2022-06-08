367e81ee9c648d4076d6109bb96cbde8968c6160
weimjsam <weimjsam@qq.com>
2022-06-08 18:24:08 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 version="21.02.3"
- ramips="mt7621"
- image_name="phicomm_k2p"
+ ramips="mt7620"
+ image_name="lenovo_newifi-y1"
  wget https://downloads.openwrt.org/releases/${version}/targets/ramips/${ramips}/openwrt-imagebuilder-${version}-ramips-${ramips}.Linux-x86_64.tar.xz
  xz -d openwrt-imagebuilder-${version}-ramips-${ramips}.Linux-x86_64.tar.xz
  tar -xvf openwrt-imagebuilder-${version}-ramips-${ramips}.Linux-x86_64.tar
