2392a45cd4955ed813a2f19c0c4430c43a343d1e
weimjsam <weimjsam@qq.com>
2023-03-12 12:16:05 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-version="22.03.3"
+version="22.03.2"
 ramips="mt7621"
 image_name="d-team_newifi-d2"
 wget https://downloads.openwrt.org/releases/${version}/targets/ramips/${ramips}/openwrt-imagebuilder-${version}-ramips-${ramips}.Linux-x86_64.tar.xz
