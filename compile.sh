3bf10385115db2646d44bb3e157c331f66b4d26b
weimjsam <weimjsam@qq.com>
2023-08-03 00:04:19 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,14 +1,14 @@
-version="22.03.5"
-mips="mt7622"
-targets="mediatek"
-image_name="xiaomi_redmi-router-ax6s"
+version="21.02.6"
+mips="mt7621"
+targets="ramips"
+image_name="d-team_newifi-d2"
 wget https://downloads.openwrt.org/releases/${version}/targets/${targets}/${mips}/openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.xz
 xz -d openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.xz
 tar -xvf openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar
 cd openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64
 
 make info
-make image PROFILE=${image_name} PACKAGES="vsftpd openssh-sftp-server uhttpd uhttpd-mod-ubus vsftpd openssh-sftp-server uhttpd uhttpd-mod-ubus luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn -logd " CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n CONFIG_KERNEL_DEBUG_KERNEL=n CONFIG_KERNEL_PRINTK_TIME=n CONFIG_COLLECT_KERNEL_DEBUG=n CONFIG_REPRODUCIBLE_DEBUG_INFO=n
+make image PROFILE=${image_name} PACKAGES="luci-app-openclash vsftpd openssh-sftp-server uhttpd uhttpd-mod-ubus vsftpd openssh-sftp-server uhttpd uhttpd-mod-ubus luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn -logd " CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n CONFIG_KERNEL_DEBUG_KERNEL=n CONFIG_KERNEL_PRINTK_TIME=n CONFIG_COLLECT_KERNEL_DEBUG=n CONFIG_REPRODUCIBLE_DEBUG_INFO=n
 
 # test：tc kmod-ifb kmod-sched kmod-sched-core kmod-netem \ luci-app-smartdns luci-i18n-smartdns-zh-cn smartdns 
 # bak ： firewall luci-i18n-firewall-zh-cn \ luci-app-openclash  kmod-inet-diag  kmod-nft-tproxy
