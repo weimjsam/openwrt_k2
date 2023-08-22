8e08e7ec26aea3e2914af7540fe9cfd6afdd3894
weimjsam <weimjsam@qq.com>
2023-08-22 14:54:32 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -8,7 +8,7 @@ tar -xvf openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar
 cd openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64
 
 make info
-make image PROFILE=${image_name} PACKAGES="tc kmod-ifb kmod-sched kmod-sched-core kmod-netem luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn wpad-openssl vsftpd openssh-sftp-server uhttpd uhttpd-mod-ubus vsftpd openssh-sftp-server uhttpd uhttpd-mod-ubus  -logd " CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n CONFIG_KERNEL_DEBUG_KERNEL=n CONFIG_KERNEL_PRINTK_TIME=n CONFIG_COLLECT_KERNEL_DEBUG=n CONFIG_REPRODUCIBLE_DEBUG_INFO=n
+make image PROFILE=${image_name} PACKAGES="tc kmod-ifb kmod-sched kmod-sched-core kmod-netem  vsftpd openssh-sftp-server uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn wpad-openssl firewall luci-i18n-firewall-zh-cn  -logd " CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n CONFIG_KERNEL_DEBUG_KERNEL=n CONFIG_KERNEL_PRINTK_TIME=n CONFIG_COLLECT_KERNEL_DEBUG=n CONFIG_REPRODUCIBLE_DEBUG_INFO=n
 
 # test：tc kmod-ifb kmod-sched kmod-sched-core kmod-netem \ luci-app-smartdns luci-i18n-smartdns-zh-cn smartdns 
 # bak ： firewall luci-i18n-firewall-zh-cn \ luci-app-openclash  kmod-inet-diag  kmod-nft-tproxy
