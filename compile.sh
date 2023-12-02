51356a1637e1cdabe695eff1e02f30dcae6019e5
weimjsam <weimjsam@qq.com>
2023-12-02 21:41:56 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 version="21.02.7"
-mips="mt7621"
+mips="mt7620"
 targets="ramips"
-image_name="d-team_newifi-d2"
+image_name="lenovo_newifi-y1"
 wget https://downloads.openwrt.org/releases/${version}/targets/${targets}/${mips}/openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.xz
 xz -d openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.xz
 tar -xvf openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar
@@ -11,16 +11,15 @@ make info
 make image PROFILE=${image_name} PACKAGES="\
 kmod-tcp-bbr uhttpd openssh-sftp-server luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn \
 -firewall4 -nftables -kmod-nft-offload firewall ip6tables iptables kmod-ipt-offload \
-python3 python3-pip \
+tc kmod-ifb kmod-sched kmod-sched-core kmod-netem \
 " CONFIG_IPV6=n CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n CONFIG_KERNEL_ELF_CORE=n CONFIG_KERNEL_DEBUG_KERNEL=n CONFIG_STRIP_KERNEL_EXPORTS=y CONFIG_KERNEL_SWAP=n CONFIG_KERNEL_PRINTK=n CONFIG_KERNEL_PRINTK_TIME=n CONFIG_COLLECT_KERNEL_DEBUG=n CONFIG_REPRODUCIBLE_DEBUG_INFO=n
 
-# test：tc kmod-ifb kmod-sched kmod-sched-core kmod-netem \ luci-app-smartdns luci-i18n-smartdns-zh-cn smartdns 
-# bak ： firewall luci-i18n-firewall-zh-cn \ luci-app-openclash  kmod-inet-diag  kmod-nft-tproxy
+# tc ### tc kmod-ifb kmod-sched kmod-sched-core kmod-netem \
 # ramips mt7620：phicomm_psg1218a   lenovo_newifi-y1  
 # ramips mt7621：phicomm_k2p   d-team_newifi-d2 
 # mediatek mt7622: xiaomi_redmi-router-ax6s
 # AP ### relayd luci-proto-relay 
-# python ### python3 python3-pip 
+# python ### python3 python3-pip screen 
 # init ### kmod-tcp-bbr uhttpd openssh-sftp-server luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn 
 # smartdns ### smartdns kmod-tcp-bbr luci-app-smartdns luci-i18n-smartdns-zh-cn
 
