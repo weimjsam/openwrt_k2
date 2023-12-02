7118c613d05a185ca2f3db777a6152fca56f9dbd
weimjsam <weimjsam@qq.com>
2023-12-02 19:44:04 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 version="23.05.2"
-mips="mt7622"
-targets="mediatek"
-image_name="xiaomi_redmi-router-ax6s"
+mips="mt7621"
+targets="ramips"
+image_name="d-team_newifi-d2"
 wget https://downloads.openwrt.org/releases/${version}/targets/${targets}/${mips}/openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.xz
 xz -d openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.xz
 tar -xvf openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar
@@ -12,7 +12,6 @@ make info
 make image PROFILE=${image_name} PACKAGES="\
 kmod-tcp-bbr uhttpd openssh-sftp-server luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn \
 -firewall4 -nftables -kmod-nft-offload firewall ip6tables iptables kmod-ipt-offload \
-relayd luci-proto-relay \
 " CONFIG_IPV6=n CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n CONFIG_KERNEL_ELF_CORE=n CONFIG_KERNEL_DEBUG_KERNEL=n CONFIG_STRIP_KERNEL_EXPORTS=y CONFIG_KERNEL_SWAP=n CONFIG_KERNEL_PRINTK=n CONFIG_KERNEL_PRINTK_TIME=n CONFIG_COLLECT_KERNEL_DEBUG=n CONFIG_REPRODUCIBLE_DEBUG_INFO=n
 
 # test：tc kmod-ifb kmod-sched kmod-sched-core kmod-netem \ luci-app-smartdns luci-i18n-smartdns-zh-cn smartdns 
@@ -20,7 +19,7 @@ relayd luci-proto-relay \
 # ramips mt7620：phicomm_psg1218a   lenovo_newifi-y1  
 # ramips mt7621：phicomm_k2p   d-team_newifi-d2 
 # mediatek mt7622: xiaomi_redmi-router-ax6s
-
+# AP ### relayd luci-proto-relay 
 # init ### kmod-tcp-bbr uhttpd openssh-sftp-server luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn 
 # smartdns ### smartdns kmod-tcp-bbr luci-app-smartdns luci-i18n-smartdns-zh-cn
 
