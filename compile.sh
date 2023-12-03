0b8c25a1e78d0e7fb8730cefc8ab5924494b39cf
weimjsam <weimjsam@qq.com>
2023-12-03 21:50:16 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -10,19 +10,18 @@ cd openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64
 make info
 make image PROFILE=${image_name} PACKAGES="\
 kmod-tcp-bbr uhttpd openssh-sftp-server luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn \
-python3 python3-pip screen \ 
+-firewall4 -nftables -kmod-nft-offload firewall ip6tables iptables kmod-ipt-offload \
+python3 python3-pip screen \
 " CONFIG_IPV6=n CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n CONFIG_KERNEL_ELF_CORE=n CONFIG_KERNEL_DEBUG_KERNEL=n CONFIG_STRIP_KERNEL_EXPORTS=y CONFIG_KERNEL_SWAP=n CONFIG_KERNEL_PRINTK=n CONFIG_KERNEL_PRINTK_TIME=n CONFIG_COLLECT_KERNEL_DEBUG=n CONFIG_REPRODUCIBLE_DEBUG_INFO=n
 
-
+# tc ### tc kmod-ifb kmod-sched kmod-sched-core kmod-netem \
 # ramips mt7620：phicomm_psg1218a   lenovo_newifi-y1  
 # ramips mt7621：phicomm_k2p   d-team_newifi-d2 
 # mediatek mt7622: xiaomi_redmi-router-ax6s
-# tc ### tc kmod-ifb kmod-sched kmod-sched-core kmod-netem \
 # AP ### relayd luci-proto-relay 
 # python ### python3 python3-pip screen 
 # init ### kmod-tcp-bbr uhttpd openssh-sftp-server luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn 
 # smartdns ### smartdns kmod-tcp-bbr luci-app-smartdns luci-i18n-smartdns-zh-cn
 
 # 21.02 ### base-files ca-bundle dropbear fstools libc libgcc libustream-wolfssl logd mtd netifd opkg uci uclient-fetch urandom-seed urngd busybox procd kmod-leds-gpio kmod-gpio-button-hotplug wpad-basic-wolfssl dnsmasq firewall ip6tables iptables kmod-ipt-offload odhcp6c odhcpd-ipv6only ppp ppp-mod-pppoe
-# 23.05 ### base-files ca-bundle dropbear fstools libc libgcc libustream-mbedtls -logd mtd netifd opkg uci uclient-fetch urandom-seed urngd busybox procd procd-ujail procd-seccomp kmod-leds-gpio kmod-gpio-button-hotplug kmod-mt7622-firmware wpad-basic-mbedtls uboot-envtools dnsmasq firewall4 nftables kmod-nft-offload odhcp6c odhcpd-ipv6only ppp ppp-mod-pppoe \
-# firewall ### -firewall4 -nftables -kmod-nft-offload firewall ip6tables iptables kmod-ipt-offload \
+# 23.05 ### base-files ca-bundle dropbear fstools libc libgcc libustream-mbedtls -logd mtd netifd opkg uci uclient-fetch urandom-seed urngd busybox procd procd-ujail procd-seccomp kmod-leds-gpio kmod-gpio-button-hotplug kmod-mt7622-firmware wpad-basic-mbedtls uboot-envtools dnsmasq firewall4 nftables kmod-nft-offload odhcp6c odhcpd-ipv6only ppp ppp-mod-
