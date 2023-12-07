afd81982eefaec3235848449a5c14c6dcee7b59c
weimjsam <weimjsam@qq.com>
2023-12-07 12:10:18 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -10,7 +10,7 @@ cd openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64
 make info
 make image PROFILE=${image_name} PACKAGES="\
 kmod-tcp-bbr uhttpd openssh-sftp-server luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn \
-iptables kmod-ipt-offload ipset iptables-nft nftset arptables-nft ebtables-nft xtables-nft \
+iptables kmod-ipt-offload ipset iptables-nft arptables-nft ebtables-nft xtables-nft \
 smartdns luci-app-smartdns luci-i18n-smartdns-zh-cn \
 " CONFIG_IPV6=n CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n CONFIG_KERNEL_ELF_CORE=n CONFIG_KERNEL_DEBUG_KERNEL=n CONFIG_STRIP_KERNEL_EXPORTS=y CONFIG_KERNEL_SWAP=n CONFIG_KERNEL_PRINTK=n CONFIG_KERNEL_PRINTK_TIME=n CONFIG_COLLECT_KERNEL_DEBUG=n CONFIG_REPRODUCIBLE_DEBUG_INFO=n
 
