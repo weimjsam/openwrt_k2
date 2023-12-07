49255affeaf308b6a8c9114ee331ccd8e28b2de5
weimjsam <weimjsam@qq.com>
2023-12-07 21:51:38 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-version="21.02.7"
+version="23.05.2"
 mips="mt7621"
 targets="ramips"
 image_name="phicomm_k2p"
@@ -10,7 +10,6 @@ cd openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64
 make info
 make image PROFILE=${image_name} PACKAGES="\
 kmod-tcp-bbr uhttpd openssh-sftp-server luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn \
-smartdns luci-app-smartdns luci-i18n-smartdns-zh-cn \
 -odhcp6c -odhcpd-ipv6only \
 relayd luci-proto-relay \
 " CONFIG_IPV6=n CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n CONFIG_KERNEL_ELF_CORE=n CONFIG_KERNEL_DEBUG_KERNEL=n CONFIG_STRIP_KERNEL_EXPORTS=y CONFIG_KERNEL_SWAP=n CONFIG_KERNEL_PRINTK=n CONFIG_KERNEL_PRINTK_TIME=n CONFIG_COLLECT_KERNEL_DEBUG=n CONFIG_REPRODUCIBLE_DEBUG_INFO=n
