9926fff1c300c764f0ea8fd0f73f83d10b9d13af
weimjsam <weimjsam@qq.com>
2023-12-09 23:01:48 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
-version="23.05.2"
+version="21.02.7"
 mips="mt7621"
 targets="ramips"
-image_name="phicomm_k2p"
+image_name="d-team_newifi-d2"
 wget https://downloads.openwrt.org/releases/${version}/targets/${targets}/${mips}/openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.xz
 xz -d openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.xz
 tar -xvf openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar
@@ -11,7 +11,7 @@ make info
 make image PROFILE=${image_name} PACKAGES="\
 kmod-tcp-bbr uhttpd openssh-sftp-server luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn \
 -odhcp6c -odhcpd-ipv6only \
-relayd luci-proto-relay \
+python3 python3-pip screen \
 " CONFIG_IPV6=n CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n CONFIG_KERNEL_ELF_CORE=n CONFIG_KERNEL_DEBUG_KERNEL=n CONFIG_STRIP_KERNEL_EXPORTS=y CONFIG_KERNEL_SWAP=n CONFIG_KERNEL_PRINTK=n CONFIG_KERNEL_PRINTK_TIME=n CONFIG_COLLECT_KERNEL_DEBUG=n CONFIG_REPRODUCIBLE_DEBUG_INFO=n
 
 # tc ### tc kmod-ifb kmod-sched kmod-sched-core kmod-netem \
