d6d96906cf453f701ca303a13060bfb90ee927bc
weimjsam <weimjsam@qq.com>
2023-12-13 00:06:44 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
-version="23.05.2"
-mips="mt7622"
-targets="mediatek"
-image_name="xiaomi_redmi-router-ax6s"
+version="21.02.7"
+mips="mt7621"
+targets="ramips"
+image_name="d-team_newifi-d2"
 wget https://downloads.openwrt.org/releases/${version}/targets/${targets}/${mips}/openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.xz
 xz -d openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.xz
 tar -xvf openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar
@@ -11,9 +11,8 @@ make info
 make image PROFILE=${image_name} PACKAGES="\
 kmod-tcp-bbr uhttpd openssh-sftp-server luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn \
 -odhcp6c -odhcpd-ipv6only \
-python3 python3-pip screen htop \
-relayd luci-proto-relay \
-redsocks2 \
+htop \
+libevent2-7 \
 " CONFIG_IPV6=n CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n CONFIG_KERNEL_ELF_CORE=n CONFIG_KERNEL_DEBUG_KERNEL=n CONFIG_STRIP_KERNEL_EXPORTS=y CONFIG_KERNEL_SWAP=n CONFIG_KERNEL_PRINTK=n CONFIG_KERNEL_PRINTK_TIME=n CONFIG_COLLECT_KERNEL_DEBUG=n CONFIG_REPRODUCIBLE_DEBUG_INFO=n
 
 # tc ### tc kmod-ifb kmod-sched kmod-sched-core kmod-netem \
