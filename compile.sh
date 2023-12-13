638a1d721759ab776cec33c5efe3f3bd019398b3
weimjsam <weimjsam@qq.com>
2023-12-13 16:26:01 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -2,17 +2,32 @@ version="21.02.7"
 mips="mt7621"
 targets="ramips"
 image_name="d-team_newifi-d2"
+
+# Download and extract the OpenWrt imagebuilder
 wget https://downloads.openwrt.org/releases/${version}/targets/${targets}/${mips}/openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.xz
 xz -d openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.xz
 tar -xvf openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar
 cd openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64
 
+# Install the necessary packages and plugins
 make info
 make image PROFILE=${image_name} PACKAGES="\
 htop kmod-tcp-bbr uhttpd openssh-sftp-server luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn \
 -odhcp6c -odhcpd-ipv6only \
-redsocks \
 " CONFIG_IPV6=n CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n CONFIG_KERNEL_ELF_CORE=n CONFIG_KERNEL_DEBUG_KERNEL=n CONFIG_STRIP_KERNEL_EXPORTS=y CONFIG_KERNEL_SWAP=n CONFIG_KERNEL_PRINTK=n CONFIG_KERNEL_PRINTK_TIME=n CONFIG_COLLECT_KERNEL_DEBUG=n CONFIG_REPRODUCIBLE_DEBUG_INFO=n
+
+# Install redsocks2 plugin
+git clone https://github.com/wltc2005/openwrt-redsocks2.git package/redsocks2
+echo "src-git redsocks2 https://github.com/wltc2005/openwrt-redsocks2.git" >> feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
+
+# Install dependencies and compile redsocks2 plugin
+./scripts/feeds update base
+./scripts/feeds install libevent2
+./scripts/feeds install ipset
+make menuconfig V=s
+make package/redsocks2/compile V=s 
+
 
 # tc ### tc kmod-ifb kmod-sched kmod-sched-core kmod-netem \
 # ramips mt7620：phicomm_psg1218a   lenovo_newifi-y1  
