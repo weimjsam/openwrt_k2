0415ab55a65db6b08ae75ec4118bbb5ac9f09ded
weimjsam <weimjsam@qq.com>
2025-01-16 10:26:53 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ mips="mt7621"
 image_name="d-team_newifi-d2"
 
 # Download and extract the OpenWrt imagebuilder
-# wget https://downloads.openwrt.org/releases/${version}/targets/${targets}/${mips}/openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.xz
+# wget https://downloads.openwrt.org/releases/${version}/targets/${targets}/${mips}/openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.xz 
 wget https://downloads.openwrt.org/releases/${version}/targets/${targets}/${mips}/openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.zst
 xz -d openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.zst
 tar -xvf openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar
