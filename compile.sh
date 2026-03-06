1c50ffc07e71bcaf260e298d872f614b3cfb536a
weimjsam <weimjsam@qq.com>
2026-03-06 19:37:48 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-version="25.12.0"
+version="24.10.5"
 targets="ramips"
 mips="mt7621"
 image_name="d-team_newifi-d2"
@@ -14,8 +14,6 @@ cd openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64
 # Install the necessary packages and plugins 
 make info
 make image PROFILE=${image_name} PACKAGES="\
-KERNEL_PATCHVER=6.6 \
-KERNEL_VERSION=6.6.119 \
 htop uhttpd openssh-sftp-server luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn bash iptables dnsmasq-full \
 curl ca-bundle ipset ip-full iptables-mod-tproxy iptables-mod-extra ruby ruby-yaml kmod-tun kmod-inet-diag unzip luci-compat luci \
 -luci-app-ddns -ddns-scripts -ddns-scripts_aliyun -ddns-scripts_dnspod -qos-scripts -kmod-usb-ehci -kmod-usb-printer -kmod-usb-storage \
@@ -27,6 +25,7 @@ curl ca-bundle ipset ip-full iptables-mod-tproxy iptables-mod-extra ruby ruby-ya
 -firewall4 -kmod-nft-offload -nftables firewall iptables-legacy ip6tables-legacy kmod-ipt-offload kmod-ipt-nat kmod-ipt-conntrack \
 libevent2-7 libopenssl3 tc-full kmod-ifb kmod-sched kmod-sched-core kmod-netem iptables-mod-conntrack-extra kmod-ipt-raw \
 mwan3 luci-app-mwan3 kmod-macvlan \
+"base-files=1693~f919e7899d" "libc=1.2.5-r5" "kernel=6.12.71~364f8debbcd4cddc1f038dea515bf8a5-r1" \
 " CONFIG_IPV6=n CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n CONFIG_KERNEL_ELF_CORE=n CONFIG_KERNEL_DEBUG_KERNEL=n CONFIG_STRIP_KERNEL_EXPORTS=y CONFIG_KERNEL_SWAP=n CONFIG_KERNEL_PRINTK=n CONFIG_KERNEL_PRINTK_TIME=n CONFIG_COLLECT_KERNEL_DEBUG=n CONFIG_REPRODUCIBLE_DEBUG_INFO=n CONFIG_OPENSSL_ENABLE_TLS1_2=y CONFIG_OPENSSL_ENABLE_TLS1_3=y CONFIG_OPENSSL_ENABLE_ALL_CIPHERS=y CONFIG_OPENSSL_ENABLE_ALL_DIGESTS=y CONFIG_OPENSSL_SECLEVEL=1 CONFIG_CRYPTO_USER_API_HASH=n CONFIG_CRYPTO_USER_API_SKCIPHER=n CONFIG_PACKAGE_ca-certificates=y CONFIG_PACKAGE_ca-bundle=y CONFIG_PACKAGE_openssl-util=y CONFIG_PACKAGE_curl=y CONFIG_PACKAGE_curl-nss=n CONFIG_PACKAGE_curl-openssl=y
 
 # htop uhttpd openssh-sftp-server luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn
