266145ff55926bcd8e6ef5479ae33cf5cbf914cb
weimjsam <weimjsam@qq.com>
2026-04-10 17:22:45 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 version="25.12.2"
-targets="ramips"
-mips="mt7621"  
-image_name="d-team_newifi-d2"
+targets="mediatek"
+mips="mt7622"  
+image_name="xiaomi_redmi-router-ax6s"
 
 # Download and extract the OpenWrt imagebuilder
 # wget https://downloads.openwrt.org/releases/${version}/targets/${targets}/${mips}/openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.xz 
@@ -10,6 +10,14 @@ wget https://downloads.openwrt.org/releases/${version}/targets/${targets}/${mips
 zstd -d openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.zst
 tar -xvf openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar
 cd openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64
+
+
+
+mkdir -p ./build_dir/target-aarch64_cortex-a53_musl/linux-mediatek_mt7622/
+wget https://github.com/weimjsam/openwrt_k2/raw/refs/heads/master/image-mt7622-xiaomi-redmi-router-ax6s.dtb \
+-O ./build_dir/target-aarch64_cortex-a53_musl/linux-mediatek_mt7622/image-mt7622-xiaomi-redmi-router-ax6s.dtb
+
+
 
 # Install the necessary packages and plugins 
 make info
@@ -28,11 +36,12 @@ libevent2-7 libopenssl3 tc-full kmod-ifb kmod-sched kmod-sched-core kmod-netem i
 mwan3 luci-app-mwan3 kmod-macvlan \
 " CONFIG_IPV6=n CONFIG_KERNEL_CRASHLOG=n CONFIG_KERNEL_DEBUG_INFO=n CONFIG_KERNEL_ELF_CORE=n CONFIG_KERNEL_DEBUG_KERNEL=n CONFIG_STRIP_KERNEL_EXPORTS=y CONFIG_KERNEL_SWAP=n CONFIG_KERNEL_PRINTK=n CONFIG_KERNEL_PRINTK_TIME=n CONFIG_COLLECT_KERNEL_DEBUG=n CONFIG_REPRODUCIBLE_DEBUG_INFO=n CONFIG_OPENSSL_ENABLE_TLS1_2=y CONFIG_OPENSSL_ENABLE_TLS1_3=y CONFIG_OPENSSL_ENABLE_ALL_CIPHERS=y CONFIG_OPENSSL_ENABLE_ALL_DIGESTS=y CONFIG_OPENSSL_SECLEVEL=1 CONFIG_CRYPTO_USER_API_HASH=n CONFIG_CRYPTO_USER_API_SKCIPHER=n CONFIG_PACKAGE_ca-certificates=y CONFIG_PACKAGE_ca-bundle=y CONFIG_PACKAGE_openssl-util=y CONFIG_PACKAGE_curl=y CONFIG_PACKAGE_curl-nss=n CONFIG_PACKAGE_curl-openssl=y
 
+# ipq807x  generic: redmi_ax6
+# mediatek mt7622: xiaomi_redmi-router-ax6s
 # htop uhttpd openssh-sftp-server luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn
 # ramips mt7620：phicomm_psg1218a、lenovo_newifi-y1  
 # ramips mt7621：phicomm_k2p、d-team_newifi-d2、xiaomi_mi-router-4a-gigabit-v2、xiaomi_mi-router-4a-gigabit
-# mediatek mt7622: xiaomi_redmi-router-ax6s
-# ipq807x  generic: redmi_ax6
+
 
 # proxy ### libevent2 iptables-nft iptables-mod-nat-extra \
 # tc ### tc kmod-ifb kmod-sched kmod-sched-core kmod-netem \
