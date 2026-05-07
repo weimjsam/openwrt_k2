763f9a2dda093677db4b4e4c44774b86235a064c
weimjsam <weimjsam@qq.com>
2026-05-07 14:09:48 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -17,10 +17,6 @@ zstd -d openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar.zst
 tar -xvf openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64.tar
 cd openwrt-imagebuilder-${version}-${targets}-${mips}.Linux-x86_64
 
-mkdir -p ./build_dir/target-aarch64_cortex-a53_musl/linux-mediatek_mt7622/
-wget https://github.com/weimjsam/openwrt-rom/raw/refs/heads/main/buildsh/ax6s.dtb \
-  -O ./build_dir/target-aarch64_cortex-a53_musl/linux-mediatek_mt7622/image-mt7622-xiaomi-redmi-router-ax6s.dtb
-
 
 # Install the necessary packages and plugins 
 make info
