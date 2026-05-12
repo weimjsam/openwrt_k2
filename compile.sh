32f43ad762cefe20f3a5b81c46c3732bccc561eb
weimjsam <weimjsam@qq.com>
2026-05-12 10:40:34 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -24,6 +24,7 @@ make info
 make -j$(nproc) image PROFILE=${image_name} PACKAGES="\
 htop uhttpd openssh-sftp-server luci-base luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn bash \
 curl ca-bundle unzip luci dnsmasq-full kmod-tcp-bbr kmod-macvlan kmod-nft-connlimit \
+libevent2-7 libopenssl3 \
 -dnsmasq -ddns-scripts -ddns-scripts_aliyun -ddns-scripts_dnspod -ip6t_REJECT -ip6tables -ip6tables-extra -ip6tables-mod-nat -ipv6helper \
 -kmod-6lowpan -kmod-ebtables-ipv6 -kmod-fou6 -kmod-gre6 -kmod-ip6-tunnel -kmod-ip6-vti -kmod-ip6tables -kmod-ip6tables-extra -kmod-ipsec6 \
 -kmod-ipt-nat6 -kmod-ipt-raw6 -kmod-iptunnel6 -kmod-ipv6 -kmod-ledtrig-usbdev -kmod-nf-conntrack6 \
